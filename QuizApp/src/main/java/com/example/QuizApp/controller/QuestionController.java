caa8e7a6243e271c53deac5d8404b100b3e9a0da
mansiiawasthi <mansiawasthi55@gmail.com>
2025-05-12 20:07:16 +0530

    Initial commit

---
+++ QuizApp/src/main/java/com/example/QuizApp/controller/QuestionController.java
@@ -0,0 +1,26 @@
+package com.example.QuizApp.controller;
+
+import com.example.QuizApp.Question;
+import com.example.QuizApp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+
+    }
+}
---
+++ QuizApp/src/main/java/com/example/QuizApp/service/QuestionService.java
@@ -0,0 +1,25 @@
+package com.example.QuizApp.service;
+
+
+import com.example.QuizApp.Question;
+import com.example.QuizApp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions(){
+         return questionDao.findAll();
+    }
+
+
+}
